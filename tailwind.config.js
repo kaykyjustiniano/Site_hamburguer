83bc2cc294570048847c92c33e4fc0837dd29a30
kaykyjustiniano <kaikiatg@gmail.com>
2023-10-22 21:27:59 -0300

    EscopoSite_NavBar-0

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    
+    fontFamily: {
+      sans: ['Bowlby One']
+
+    },
+    extend: {
+    }
+  }
+},
+{
+  plugins: [],
+}
+
